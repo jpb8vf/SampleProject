ee8a8473a6142a42bfd6f9663861eddc3784b65a
Vito Bertolino <vitobertolino@Vitos-iMac.local>
2018-07-25 20:05:34 -0500

    first commit

---
+++ SampleProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by Vito Bertolino on 7/25/18.
+//  Copyright © 2018 Joseph Bertolino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
